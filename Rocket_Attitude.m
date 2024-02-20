2427291c5969f5ff87d3f4f807a2c46798d73c50
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2024-02-20 14:12:23 -0800

    Rocket_Attitude.m initial commit

---
+++ Rocket_Attitude.m
@@ -0,0 +1,21 @@
+clear
+
+I = 5000; % Moment of Inertia [m^4]
+
+A = [0, 1; 0, 0];
+
+B = [0; 1/I];
+
+
+Kp = 1;
+Ki = 0;
+Kd = 1;
+
+s = tf('s');
+C = Kp + Ki/s + Kd*s
+
+% C = pid(Kp,Ki,Kd) % this does the same thing
+
+
+
+
